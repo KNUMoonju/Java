00764d06b942a1492c2eb1d901383964e59ea56b
mjung <mjung@172.16.110.200>
2017-11-13 15:25:11 -0500

    UI 수정 조금 함

--- src/UI.java
+++ src/UI.java
@@ -16,7 +16,14 @@ public class UI {
 		System.out.println("문주가 추가 다시함");
 	}
 	
-	public User create_user() {
+	//TODO 유저 로그인할 때 파일에서 유저목록을 읽어와서 해당 유저를 찾아낸다.
+	private User login_user() {
+		User n = new User("a","b","c","d","e");
+		
+		return n;
+	}
+	
+	private User create_user() {
 		System.out.println("유저 생성");
 		System.out.println("GIT TEST");
 		
@@ -47,15 +54,24 @@ public class UI {
 		
 		booting();
 		
-		System.out.println("메뉴선택");
-		System.out.println("1 : 유저 생성");
+		System.out.println("* 메뉴선택\t\t\t\t\t\t\t\t*");
+		System.out.println("* 1 : 유저 로그인\t\t\t\t\t\t\t*");
+		System.out.println("* 2 : 유저 생성\t\t\t\t\t\t\t*");
 		
+		//메뉴가 추가될 때 여기에 추가하면 된다.
+		
+		System.out.println("*\t\t\t\t\t\t\t\t*");
+		System.out.println("*****************************************************************");
+		System.out.print("> ");
 		int inp_menu = get_int();
 		
 		System.out.println("입력 : " + inp_menu);
 		
 		switch(inp_menu) {
 		case 1:
+			login_user();
+			break;
+		case 2:
 			create_user();
 			break;
 		}
@@ -101,25 +117,25 @@ public class UI {
 		System.out.println("*****************************************************************");
 		System.out.println("*\t\t\t\t\t\t\t\t*");
 		System.out.print("*\t\t\t");
-		sleep_msec(100);
+		sleep_msec(150);
 		System.out.print("3");
-		sleep_msec(100);
+		sleep_msec(150);
 		System.out.print("조");
-		sleep_msec(100);
+		sleep_msec(150);
 		System.out.print("재");
-		sleep_msec(100);
+		sleep_msec(150);
 		System.out.print("고");
-		sleep_msec(100);
+		sleep_msec(150);
 		System.out.print("관");
-		sleep_msec(100);
+		sleep_msec(150);
 		System.out.print("리");
-		sleep_msec(100);
+		sleep_msec(150);
 		System.out.print(" 시");
-		sleep_msec(100);
+		sleep_msec(150);
 		System.out.print("스");
-		sleep_msec(100);
+		sleep_msec(150);
 		System.out.println("템\t\t\t\t*");
-		System.out.println("");
+		System.out.println("*\t\t\t\t\t\t\t\t*");
 	}
 	
 	private void clear_screen() {	//화면지우기.
